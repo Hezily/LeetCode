c5f28c1181b83c119f8cbd2d7dee67d62f1d94b0
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2025-01-11 07:59:37 +0530

    Time: 0 ms (100%), Space: 17.9 MB (14.41%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Initialize pointers
+        low, mid, high = 0, 0, len(nums) - 1
+        
+        # Iterate through the array
+        while mid <= high:
+            if nums[mid] == 0:  # Red color
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:  # White color
+                mid += 1
+            else:  # Blue color
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
