932000c075aaa9f5e9209964859389c79af4b4a0
Rahul 22BCS10162 <108000914+Hezily@users.noreply.github.com>
2025-02-21 16:05:50 +0530

    Time: 3 ms (93.05%), Space: 22.3 MB (37.43%) - LeetHub

---
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class FindElements:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.set = set()
+        self.dfs(root, 0)
+
+    def find(self, target: int) -> bool:
+        return target in self.set
+
+    def dfs(self, root: Optional[TreeNode], val: int):
+        if not root:
+            return
+        root.val = val
+        self.set.add(val)
+        self.dfs(root.left, 2 * val + 1)
+        self.dfs(root.right, 2 * val + 2)
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
