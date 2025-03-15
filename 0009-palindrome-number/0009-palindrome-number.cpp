23ff960ed076edc30f229675d2fc735d0bc2355a
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2025-03-16 00:29:04 +0530

    Time: 0 ms (100%), Space: 8.6 MB (68.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+        int originalX = x;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+};
