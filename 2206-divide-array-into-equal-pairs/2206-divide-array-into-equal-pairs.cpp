485aeea0799911d3013d549b02a647ce858bebe8
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2025-03-17 21:30:29 +0530

    Time: 3 ms (58.51%), Space: 16.7 MB (84.67%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
-        int freq[1001] = {0}; 
+        // int freq[1001] = {0}; 
 
-        for (int num : nums) {
-            freq[num]++;
-        }
+        // for (int num : nums) {
+        //     freq[num]++;
+        // }
 
-        for (int i = 0; i <= 1000; i++) {
-            if (freq[i] % 2 != 0) {
+        // for (int i = 0; i <= 1000; i++) {
+        //     if (freq[i] % 2 != 0) {
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size(); i += 2){
+            if(nums[i] != nums[i+1]){
                 return false;
             }
         }
-
         return true;
     }
 };
