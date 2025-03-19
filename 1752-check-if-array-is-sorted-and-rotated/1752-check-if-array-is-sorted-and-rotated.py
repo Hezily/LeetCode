a718ecf8db8322ca15da29dc705589c1d0766e35
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2025-03-19 20:28:44 +0530

    Time: 1 ms (15.44%), Space: 17.7 MB (85.29%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -4,7 +4,7 @@ class Solution:
         n = len(nums)
         
         for i in range(n):
-            if nums[i] > nums[(i + 1) % n]:  # Check if the current element is greater than the next one
+            if nums[i] > nums[(i + 1) % n]: 
                 count += 1
         
-        return count <= 1  # If there's at most one descent, the array can be rotated and sorted
+        return count <= 1
