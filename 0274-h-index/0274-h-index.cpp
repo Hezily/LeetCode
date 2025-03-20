c240d085a1982e25a1787e33b029aa0754753d7c
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2025-03-20 22:07:25 +0530

    Time: 0 ms (100%), Space: 13.7 MB (7.67%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -7,11 +7,8 @@ public:
         for (int i = 0; i < citations.size(); i++) {
             if (citations[i] >= i + 1) {
                 h = i + 1;
-            } else {
-                break;
-            }
+            } 
         }
-        
         return h;
     }
 };
