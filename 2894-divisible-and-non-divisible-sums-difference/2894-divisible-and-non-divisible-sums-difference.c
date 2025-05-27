d67645e0280e78f523bf23b58fc057738b174b05
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2025-05-27 19:45:04 +0530

    Time: 0 ms (100%), Space: 8.2 MB (75.93%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.c
@@ -0,0 +1,6 @@
+int differenceOfSums(int n, int m) {
+    int totalSum = n * (n + 1) / 2;
+    int k = n / m;
+    int divisibleSum = m * k * (k + 1) / 2;
+    return totalSum - 2 * divisibleSum;
+}
