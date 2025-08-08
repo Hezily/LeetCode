5ff194e80c2217e7645fad43ea2b62954f9ef06b
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2025-08-08 23:56:20 +0530

    Time: 23 ms (46%), Space: 14.9 MB (47.67%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    unordered_map<string, double> memo;
+
+    double solve(int x, int y) {
+        if (x <= 0 && y <= 0) return 0.5;
+        if (x <= 0) return 1.0;
+        if (y <= 0) return 0.0;
+
+        string key = to_string(x) + "*" + to_string(y);
+        if (memo.count(key)) return memo[key];
+
+        double ans = solve(x - 100, y) +
+                     solve(x - 75, y - 25) +
+                     solve(x - 50, y - 50) +
+                     solve(x - 25, y - 75);
+
+        memo[key] = ans * 0.25;
+        return memo[key];
+    }
+
+public:
+    double soupServings(int n) {
+        if (n > 4500) return 1.0;
+        return solve(n, n);
+    }
+};
