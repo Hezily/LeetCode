077a3a5b6796bdbf736b27fe4940c3fa99b200cf
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2026-01-01 23:31:42 +0530

    Time: 0 ms (100%), Space: 11.5 MB (50.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -5,13 +5,12 @@ public:
         
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
-                digits[i]++;       // just increment and return if no carry
+                digits[i]++; 
                 return digits;
             }
-            digits[i] = 0;         // set to 0 and carry over
+            digits[i] = 0;
         }
         
-        // If all digits were 9, we need an extra digit at the front
         digits.insert(digits.begin(), 1);
         return digits;
     }
