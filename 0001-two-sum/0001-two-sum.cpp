05a1fbbd60b5c07ee03c52fb0d90226b14e3340f
Hezily Quasar <108000914+Hezily@users.noreply.github.com>
2026-01-07 20:36:00 +0530

    Time: 4 ms (53.1%), Space: 14.9 MB (19.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> v;
+    //     vector<int> v;
 
-        for(int i = 0; i < nums.size(); i++)
+    //     for(int i = 0; i < nums.size(); i++)
+    //     {
+    //         for(int j = i + 1; j < nums.size(); j++)
+    //         {
+    //             if(nums[i] + nums[j] == target)
+    //             {
+    //                 v.push_back(i);
+    //                 v.push_back(j);
+    //             }
+    //         }
+    //     }
+    //     return v;
+    // }
+
+    map<int, int> mp;
+
+    for(int i = 0; i < nums.size(); i++)
+    {
+        int need = target - nums[i];
+
+        if(mp.find(need) != mp.end())
         {
-            for(int j = i + 1; j < nums.size(); j++)
-            {
-                if(nums[i] + nums[j] == target)
-                {
-                    v.push_back(i);
-                    v.push_back(j);
-                }
-            }
+            return {mp[need], i};
         }
 
-        return v;
+        mp[nums[i]] = i;
+    }
+
+    return {-1, -1};
     }
 };
